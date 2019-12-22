9d87e298634fd83acb1565194851a1ec588a2a08
Gabriel Bernardi <50278200+gabrielrbernardi@users.noreply.github.com>
2019-12-21 23:34:45 -0300

    Adicionado funcionalidade de login
    Neste commit foi adicionada a funcionalidade de login no sistema

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,55 +1,81 @@
-"use strict"
 const Usuario = require('../models/usuario');
+const bcrypt = require('bcrypt');
+const saltRound = 10;
+const myPlaintextPassword = 's0P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+// const teste = "$2b$10$zI1j4WgwyO/VA5iYkAwawu8Io8qmwHwDsEoundkrwU/pCdyJ4yV9e";
 
-module.exports = {
-    create(req, res) {
-        const { Nome, Matricula, Email, Senha } = req.body
-        Usuario.create({ Nome, Matricula, Email, Senha })
-            .then( usuario => res.json(usuario))
-            .catch( err => res.json(err))
-    },
+exports.create = function(req, res){
+    // The 'Matricula' and 'Senha' will passsed to hash function    
+    var hash = bcrypt.hashSync(req.body.Senha, saltRound);      //function that generate hash which will be stored on DB
+    Usuario.create({
+        Nome: req.body.Nome,
+        Matricula: req.body.Matricula,
+        Email: req.body.Email,
+        Senha: hash
+    }).then(function(usuario){
+        res.send(usuario);
+    }).catch(function(err){
+        res.send(err);
+    });
+};
 
-    findOne(req, res) {
-        const { id } = req.params
-        Usuario.findOne({ id })
-            .then( usuario => res.json(usuario))
-            .catch( err => res.json(err))
-    },
-    
-    findAll(req, res) {
-        const limit = 10; 
-        const offset = (parseInt(req.params.page) - 1) * limit;
+exports.findOne = function(req, res){
+    Usuario.findById(req.params.id).then(function(usuario){
+        res.send(usuario);
+    }).catch(function(err){
+        res.send(err);
+    });
+};
 
-        Usuario.findAll({
-            offset,
-            limit
+exports.findAll = function(req, res){
+    Usuario.findAll().then(function(usuarios){
+        res.send(usuarios);
+    }).catch(function(err){
+        res.send(err);
+    });
+};
+
+exports.update = function(req, res){
+    Usuario.update({
+        Nome: req.body.Nome,
+        Matricula: req.body.Matricula,
+        Email: req.body.Email,
+        Senha: req.body.Senha
+    }, {where: {id: req.params.id}}).then(function(){
+        res.status(200).send('Usuario atualizado com sucesso.');
+    }).catch(function(err){
+        res.send(err);
+    });
+};
+
+exports.delete = function(req, res){
+    Usuario.destroy({
+        where: {id: req.params.id}
+    }).then(function(){
+        res.status(200).send('Usuario deletado com sucesso');
+    }).catch(function(err){
+        res.send(err);
+    });
+};
+
+exports.login = function(req, res){
+    nomeBody = req.body.Nome
+    matriculaBody = req.body.Matricula
+    emailBody = req.body.Email
+    senhaBody = req.body.Senha
+
+    Usuario.findOne({
+        where: {
+            Matricula: matriculaBody
+        }
+    }).then(function(usuario){
+        bcrypt.compare(senhaBody, usuario.Senha, function(err, resposta){
+            if(resposta){
+                res.send({authorizedLogin: true})
+            }else{
+                res.send({authorizedLogin: false})
+            }
         })
-            .then( usuarios => res.json(usuarios))
-            .catch( err => res.json(err))
-    },
-
-    update(req, res) {
-        const { Nome, Matricula, Email, Senha } = req.body
-        const { id } = req.params
-        Usuario.update({ Nome, Matricula, Email, Senha }, {where: {id}})
-            .then( usuario => {
-                if (usuario[0] !== 0)
-                    res.status(200).json({ result: "Usuário atualizado com sucesso."})
-                else
-                    res.status(200).json({ result: "Usuário não encontrado."})
-            })
-            .catch( err => res.json(err))
-    },
-
-    delete(req, res) {
-        const { id } = req.params
-        Usuario.destroy({where: {id}})
-            .then( usuario => {
-                if (usuario !== 0)
-                    res.status(200).json({ result: "Usuário deletado com sucesso."})
-                else
-                    res.status(200).json({ result: "Usuário não encontrado."})
-            })
-            .catch( err => res.json(err))
-    }
+    })
 }
