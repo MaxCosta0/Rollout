168b7f7a1a3ec6ff17cfe2b11528d80feefa2d22
gabrielrbernardi <gabrielrbernardi@gmail.com>
2019-12-23 02:19:34 -0300

    Added files via upload

--- models/usuario.js
+++ models/usuario.js
@@ -4,7 +4,8 @@ const Usuario = db.sequelize.define('usuario', {
     Nome: {type: db.Sequelize.STRING, allowNull: false},
     Matricula: {type: db.Sequelize.STRING, allowNull: false},
     Email: {type: db.Sequelize.STRING, allowNull: false},
-    Senha: {type: db.Sequelize.STRING, allowNull: false}
+    Senha: {type: db.Sequelize.STRING, allowNull: false},
+    isVerified: {type: db.Sequelize.BOOLEAN, allowNull: false}
 }, {
     freezeTableName: true
 });
