970bc0a7cf3176343d0212ceee630dccc0592a97
Gabriel Bernardi <50278200+gabrielrbernardi@users.noreply.github.com>
2019-12-25 02:38:50 -0300

    Novas rotas adicionadas

--- routes/usuario.js
+++ routes/usuario.js
@@ -5,8 +5,11 @@ let controller = require('../controllers/usuario');
 
 router.post('/usuario/create', controller.create);
 router.get('/usuario/:id', controller.findOne);
-router.get('/usuarios/:page', controller.findAll);
+router.get('/usuario', controller.findAll);
 router.put('/usuario/:id/update', controller.update);
 router.delete('/usuario/:id/delete',controller.delete);
+router.post('/usuario/login', controller.login);
+router.post('/usuario/verifyToken/:token', controller.verifyToken);
+router.post('/usuario/sendTokenAgain', controller.sendTokenAgain);
 
 module.exports = router;
